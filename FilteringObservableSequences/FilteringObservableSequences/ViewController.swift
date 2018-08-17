c59ac3f9b264ff48297c3de21b07cd5912f892c8
timmybea <tim.beals@gmail.com>
2018-08-17 13:20:37 -0700

    basic setup of new project: Filtering Observable Sequences

---
+++ FilteringObservableSequences/FilteringObservableSequences/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FilteringObservableSequences
+//
+//  Created by Tim Beals on 2018-08-17.
+//  Copyright © 2018 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.blue
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
